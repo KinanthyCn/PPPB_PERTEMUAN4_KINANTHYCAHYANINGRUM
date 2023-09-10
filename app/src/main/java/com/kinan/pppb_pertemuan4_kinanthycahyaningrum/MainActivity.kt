d6dd07895968400093bb836126b83565a2c2f6cf
KinanthyCn <kinanthycahyaningrum@mail.ugm.ac.id>
2023-09-10 21:19:47 +0700

    Initial commit

---
+++ app/src/main/java/com/kinan/pppb_pertemuan4_kinanthycahyaningrum/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kinan.pppb_pertemuan4_kinanthycahyaningrum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
